c4f8fb6d3934a68e794f4c9732e234b5ad3fd657
mustafa <mustafa.gulzar@kcl.ac.uk>
2023-01-26 18:23:58 +0000

    added pre process and updated chatbott

--- chatbot.py
+++ chatbot.py
@@ -1,16 +1,40 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
+# from pprint import pprint
+# import nltk
+# import json
+# nltk.download('stopwords')
+# from Questgen import main
+
+# f = open('payload.json')
+# payload = json.load(f)
+
+# payload = {
+#     "input_text": payload['text']
+# }
+
+# qg = main.QGen()
+# output = qg.predict_shortq(payload)
+# pprint(output)
 
 # Create a new chatbot
 chatbot = ChatBot("Educational Chatbot")
 
 # Create a new trainer
-trainer = ChatterBotCorpusTrainer(chatbot)
+corpus_trainer = ChatterBotCorpusTrainer(chatbot)
 
 # Now let's train the bot on the english corpus
-trainer.train("chatterbot.corpus.english.greetings",
+corpus_trainer.train("chatterbot.corpus.english.greetings",
+              "chatterbot.corpus.english",
               "chatterbot.corpus.english.conversations")
 
+list_trainer = ListTrainer(chatbot)
+list_trainer.train([
+    "What is the main theme of the book?", "The main theme of the book is X",
+    "Who is the author of the book?", "The author of the book is Y",
+    "When was the book published?", "The book was published in Z",
+    "Hello, how are you today?", "I am really W, how about K"
+    ])
 # Get a response to an input statement
 response = chatbot.get_response("Hello, how are you today?")
 print(response)
---
+++ preproccess.py
@@ -0,0 +1,4 @@
+#this script uses the questgen api to render questions which will be used in training the chatbot
+#first we will use summarize_bot to generate cliffnotes
+#this will then be used as input for the questgen to generate question answer pairs and then be used to train the chatbot
+#these Q/A pairs can be used to create a FAQ page later on
